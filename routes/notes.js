259f47c47169c53dd51f3a5b2ac5c8cf070e11ea
yoshi <yoshi@Mac.local>
2024-08-03 16:53:36 +0900

    add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri ="**";
+const uri ="mongodb+srv://burukumi2000:XlFkHnNoFXIPaTqJ@yosi.pcluqqm.mongodb.net/?retryWrites=true&w=majority&appName=yosi";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
